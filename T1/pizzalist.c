9fbaea656336fe4e28225159cc2e4049a6096c13
Bastian Mavrakis <bamavrakis@uc.cl>
2015-09-13 17:09:55 -0300

    Mas encadenamiento

--- T1/pizzalist.c
+++ T1/pizzalist.c
@@ -72,6 +72,7 @@ void pizzalist_addlast(Pizzalist *list, void *ValueToAdd, char *key)
         exit(EXIT_FAILURE);
     }
     memcpy(element->value, ValueToAdd, list->elementsize);
+    memcpy(element->key, key, sizeof(char));
 
 
     // the next element is null because we add at the end of the list.
@@ -109,7 +110,7 @@ void pizzalist_addbeg(Pizzalist *list, void *ValueToAdd, char *key)
     }
     memcpy(element->value, ValueToAdd, list->elementsize);
     memcpy(element->key, key, sizeof(char));
-    memcpy(element->keysize, keysize, sizeof(int));
+
 
 
 

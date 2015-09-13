da069f13881b41d6c00b4dae53d82e54e7fbeac5
Bastian Mavrakis <bamavrakis@uc.cl>
2015-09-13 20:00:46 -0300

    sigue

--- T1/testbua.c
+++ T1/testbua.c
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[]) {
   for(i=0;i<cantidad;i++)
   {
     printf("NOOOO");
-    array_item_at(hash, 1,list);
+    array_item_at(hash, closed_addressing_hashing(hash, (unsigned char *)&(pizza[0])),list);
     // en vez de 1  closed_addressing_hashing(hash, (unsigned char *)&(pizza[0]))
     //if(!(pizzalist_find(list,queue,&(pizza[0]))))
     //{

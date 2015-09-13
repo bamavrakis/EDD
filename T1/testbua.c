7b27f1290fa2af3af186fb35979157618b257aac
Bastian Mavrakis <bamavrakis@uc.cl>
2015-09-13 19:25:08 -0300

    aaaaaaa

--- T1/testbua.c
+++ T1/testbua.c
@@ -6,5 +6,8 @@
 #include "queuepizza.h" /* Malloc duderino */
 int main(int argc, char const *argv[]) {
   Array *hash = array_init(sizeof(Pizzalist),pizzalist_destroy);
+  scanf("%d", &cantidad);
+  printf("%d",cantidad);
+
   return 0;
 }

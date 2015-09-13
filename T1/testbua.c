4437209cfe87b7098df70567534c681e408de30b
Bastian Mavrakis <bamavrakis@uc.cl>
2015-09-13 18:48:18 -0300

    prueba

---
+++ T1/testbua.c
@@ -0,0 +1,11 @@
+#include <stdio.h> /* Standard Input Output */
+#include <string.h> /* operaciones de strings */
+#include <stdlib.h>
+#include "pizzalist.h"
+#include "closedhashing.h"
+#include "queuepizza.h" /* Malloc duderino */
+int main(int argc, char const *argv[]) {
+  printf("ya pue");
+  Array *hash = array_init(sizeof(Pizzalist),pizzalist_destroy);
+  return 0;
+}
